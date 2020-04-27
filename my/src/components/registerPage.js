15baba5bd66c10f7615fc4231a5c2e9a0fafaa04
GraceeElton <60789216+GraceeElton@users.noreply.github.com>
2020-04-27 15:13:57 -0600

    added styling to signup page

---
+++ my/src/components/registerPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Register from "./Register";
+import "../styles/registerPage.css";
+
+export default function Signup() {
+  return (
+    <div className="whole">
+      <div className="Holder">
+        <Register />
+        <div>oh no</div>
+      </div>
+    </div>
+  );
+}
