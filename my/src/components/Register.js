a60dd4fb904a9dc23c2ee7ec636e184289215a0c
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-28 10:37:44 -0500

    Update after branchout

--- my/src/components/Register.js
+++ my/src/components/Register.js
@@ -1,37 +1,37 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 export default function Register() {
-  const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
+	const { register, handleSubmit, errors } = useForm();
+	const onSubmit = (data) => console.log(data);
+	console.log('Register error', errors);
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Link to={"/"}>
-        <div>Login</div>
-      </Link>
-      <input
-        type="text"
-        placeholder="User Name"
-        name="User Name"
-        ref={register({ required: true, min: 2, maxLength: 80 })}
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        name="Email"
-        ref={register({ required: true, pattern: /^\S+@\S+$/i })}
-      />
-      <input
-        type="text"
-        placeholder="Password"
-        name="Password"
-        ref={register({ required: true, min: 1, maxLength: 12 })}
-      />
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<Link to={'/'}>
+				<div>Login</div>
+			</Link>
+			<input
+				type="text"
+				placeholder="User Name"
+				name="User Name"
+				ref={register({ required: true, min: 2, maxLength: 80 })}
+			/>
+			<input
+				type="text"
+				placeholder="Email"
+				name="Email"
+				ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+			/>
+			<input
+				type="text"
+				placeholder="Password"
+				name="Password"
+				ref={register({ required: true, min: 1, maxLength: 12 })}
+			/>
 
-      <input type="submit" />
-    </form>
-  );
+			<input type="submit" />
+		</form>
+	);
 }
