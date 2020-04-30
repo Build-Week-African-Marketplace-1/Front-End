1b224f35b7d429125166126a9aed80a7e2b9c25c
Lambda_School_Loaner_277 <lambda_school_loaner_277@Lambda-School-Loaner-277s-MacBook-Pro.local>
2020-04-30 12:25:23 -0400

    Updated Merchandise Form

--- my/src/components/Merchandise.js
+++ my/src/components/Merchandise.js
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
+
 import React from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
 
 export default function Merchandise() {
   const { register, handleSubmit, errors } = useForm();
@@ -9,30 +10,32 @@ export default function Merchandise() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Link to={"/Register"}>Register</Link>
       <input
         type="text"
         placeholder="User Name"
         name="User Name"
-        ref={register({ required: true, min: 2, maxLength: 80 })}
+        ref={register({ required: true, maxLength: 80 })}
       />
-
+      <select name="Location" ref={register({ required: true })}>
+        <option value="Nigeria ">Nigeria </option>
+        <option value="South Africa ">South Africa </option>
+        <option value="West Africa ">West Africa </option>
+      </select>
       <input
         type="number"
-        placeholder="quantity"
-        name="quantity"
-        ref={register({ required: true, min: 1 })}
+        placeholder="Quantity"
+        name="Quantity"
+        ref={register({ required: true, pattern: /^\S+@\S+$/i })}
       />
       <input
-        type="number"
+        type="text"
         placeholder="Price"
         name="Price"
-        ref={register({ required: true })}
+        ref={register({ required: true, maxLength: 12 })}
       />
       <textarea
-        name="description "
-        placeholder="description"
-        ref={register({ required: true, min: 1, maxLength: 300 })}
+        name="Description"
+        ref={register({ required: true, maxLength: 300 })}
       />
 
       <input type="submit" />
