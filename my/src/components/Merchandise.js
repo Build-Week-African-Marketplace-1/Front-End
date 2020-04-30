c40e3814f283bf58a14839845668921851f8b2fb
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-04-30 14:04:53 -0500

    minor changes

--- my/src/components/Merchandise.js
+++ my/src/components/Merchandise.js
@@ -1,89 +1,90 @@
-import React, { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
-import { Link, useParams, useHistory } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { Link, useParams, useHistory } from 'react-router-dom';
 
 const initialItem = {
-  name: "",
-  price: "",
-  description: "",
-  quantity: "",
+	name: '',
+	price: '',
+	description: '',
+	quantity: '',
 };
 
 export default function Merchandise(props) {
-  const { register } = useForm();
-  const { push } = useHistory();
-  const { id } = useParams();
-  const [newItem, setNewItem] = useState(initialItem);
+	const { register } = useForm();
+	const { push } = useHistory();
+	const { id } = useParams();
+	const [newItem, setNewItem] = useState(initialItem);
 
-  console.log("Thsi is the new item", newItem);
-  console.log("Thsi is the new item", setNewItem);
+	console.log('Thsi is the new item', newItem);
+	console.log('Thsi is the new item', setNewItem);
 
-  useEffect(() => {
-    axiosWithAuth()
-      .get(`/api/products/${id}`)
+	useEffect(() => {
+		axiosWithAuth()
+			.get(`/api/products/${id}`)
 
-      .then((res) => {
-        console.log({ res });
-        setNewItem(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [id]);
-  console.log(id);
-  const handleChange = (e) => {
-    setNewItem({ ...newItem, [e.target.name]: e.target.value });
-  };
+			.then((res) => {
+				console.log({ res });
+				setNewItem(res.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, [id]);
+	console.log(id);
+	const handleChange = (e) => {
+		setNewItem({ ...newItem, [e.target.name]: e.target.value });
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axiosWithAuth()
-      .put(`/api/products/${id}`, newItem)
-      .then((res) => {
-        console.log({ res });
-        setNewItem(res.data);
-        push("/products");
-      })
-      .catch((err) => console.log(err));
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		axiosWithAuth()
+			.put(`/api/products/${id}`, newItem)
+			.then((res) => {
+				console.log({ res });
+				setNewItem(res.data);
+				push('/products');
+			})
+			.catch((err) => console.log(err));
+	};
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="name"
-        name="name"
-        onChange={handleChange}
-        value={newItem.name}
-        ref={register({ required: true, min: 2, maxLength: 80 })}
-      />
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type="text"
+				placeholder="name"
+				name="name"
+				onChange={handleChange}
+				value={newItem.name}
+				ref={register({ required: true, min: 2, maxLength: 80 })}
+			/>
 
-      <input
-        type="number"
-        placeholder="quantity"
-        name="quantity"
-        onChange={handleChange}
-        value={newItem.quantity}
-        ref={register({ required: true, min: 1 })}
-      />
-      <input
-        type="number"
-        placeholder="Price"
-        name="price"
-        onChange={handleChange}
-        value={newItem.price}
-        ref={register({ required: true })}
-      />
-      <textarea
-        name="description "
-        onChange={handleChange}
-        placeholder="description"
-        value={newItem.description}
-        ref={register({ required: true, min: 1, maxLength: 300 })}
-      />
+			<input
+				type="number"
+				placeholder="quantity"
+				name="quantity"
+				onChange={handleChange}
+				value={newItem.quantity}
+				ref={register({ required: true, min: 1 })}
+			/>
+			<input
+				type="number"
+				placeholder="Price"
+				name="price"
+				onChange={handleChange}
+				value={newItem.price}
+				ref={register({ required: true })}
+			/>
 
-      <input type="submit" />
-    </form>
-  );
+			<textarea
+				name="description "
+				onChange={handleChange}
+				placeholder="description"
+				value={newItem.description}
+				ref={register({ required: true, min: 1, maxLength: 300 })}
+			/>
+
+			<input type="submit" />
+		</form>
+	);
 }
